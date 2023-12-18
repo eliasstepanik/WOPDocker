613ef18f69c2ebe98f9f585003a3f8db24d95c86
Elias Stepanik <40958815+eliasstepanik@users.noreply.github.com>
2023-12-18 20:17:24 +0100

    Update start-wop.sh

--- start-wop.sh
+++ start-wop.sh
@@ -2,4 +2,4 @@
 
 
 #cd /wop/server/worldofpadman-1-6-2/
-./data/wopded-linux.x86_64 +set dedicated 1 +set net_port 27960 +set com_hunkMegs 256 +exec server-settings.cfg
+./data/wopded-linux.x86_64 +set dedicated 2 +set net_port 27960 +set com_hunkMegs 1024 +exec server-settings.cfg
